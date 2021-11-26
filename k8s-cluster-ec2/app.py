756de89013699f3b4e4840a280dd2afb75d999fa
Donnie Prakoso <donnie.prakoso@gmail.com>
2021-11-27 01:39:00 +0800

    Adding pipeline

--- k8s-cluster-ec2/app.py
+++ k8s-cluster-ec2/app.py
@@ -3,6 +3,7 @@ from aws_cdk import core as cdk
 
 from k8s_cluster.k8s_cluster_stack import K8SClusterStack
 from k8s_cluster.k8s_container_stack import K8SContainerStack
+from k8s_cluster.k8s_pipeline_stack import K8SPipelineStack
 
 AWS_ACCOUNT_ID = cdk.Aws.ACCOUNT_ID
 AWS_REGION = cdk.Aws.REGION
@@ -10,9 +11,16 @@ STACK_NAME = "cdk8s-eks-ec2"
 
 app = cdk.App()
 cluster = K8SClusterStack(app, STACK_NAME,
-    env=cdk.Environment(account=AWS_ACCOUNT_ID, region=AWS_REGION),
-    )
+                          env=cdk.Environment(
+                              account=AWS_ACCOUNT_ID, region=AWS_REGION),
+                          )
 
 container = K8SContainerStack(app, "{}-container".format(STACK_NAME))
 container.add_dependency(cluster)
+
+pipeline = K8SPipelineStack(app, "{}-pipeline".format(STACK_NAME),
+                            env=cdk.Environment(
+                                account=AWS_ACCOUNT_ID, region=AWS_REGION),
+                            )
+
 app.synth()
---
+++ k8s-cluster-ec2/k8s_cluster/k8s_pipeline_stack.py
@@ -0,0 +1,32 @@
+from aws_cdk import core
+from aws_cdk import aws_codepipeline as codepipeline
+from aws_cdk import aws_codepipeline_actions as cpactions
+from aws_cdk import pipelines
+
+
+class K8SPipelineStack(core.Stack):
+    def __init__(self, scope: core.Construct, id: str, **kwargs):
+        super().__init__(scope, id, **kwargs)
+
+        source_artifact = codepipeline.Artifact()
+        cloud_assembly_artifact = codepipeline.Artifact()
+
+        pipeline = pipelines.CdkPipeline(self, 'Pipeline',
+
+                                         cloud_assembly_artifact=cloud_assembly_artifact,
+                                         pipeline_name='cdk8s-pipeline',
+
+                                         source_action=cpactions.GitHubSourceAction(
+                                             action_name='Github',
+                                             output=source_artifact,
+                                             oauth_token=core.SecretValue.secrets_manager(
+                                                 'github-token'),
+                                             owner='donnieprakoso',
+                                             repo='tutorial-cdk-eks-ec2',
+                                             trigger=cpactions.GitHubTrigger.POLL),
+
+                                         synth_action=pipelines.SimpleSynthAction(
+                                             source_artifact=source_artifact,
+                                             cloud_assembly_artifact=cloud_assembly_artifact,
+                                             install_command='npm install -g aws-cdk && pip install -r requirements.txt',
+                                             synth_command='cdk synth'))
