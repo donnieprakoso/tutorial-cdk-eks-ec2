9a16097279b6bdbe3cc282f55c48f7b955f479b2
Donnie Prakoso <donnie.prakoso@gmail.com>
2021-11-27 12:02:56 +0800

    Upgrading to latest API

--- k8s-cluster-ec2/k8s_cluster/k8s_pipeline_stack.py
+++ k8s-cluster-ec2/k8s_cluster/k8s_pipeline_stack.py
@@ -1,6 +1,5 @@
 from aws_cdk import core
-from aws_cdk import aws_codepipeline as codepipeline
-from aws_cdk import aws_codepipeline_actions as cpactions
+from aws_cdk.pipelines import CodePipeline, CodePipelineSource, ShellStep
 from aws_cdk import pipelines
 
 
@@ -8,26 +7,18 @@ class K8SPipelineStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs):
         super().__init__(scope, id, **kwargs)
 
-        source_artifact = codepipeline.Artifact()
-        cloud_assembly_artifact = codepipeline.Artifact()
-
-        pipeline = pipelines.CdkPipeline(self, 'Pipeline',
-
-                                         cloud_assembly_artifact=cloud_assembly_artifact,
-                                         pipeline_name='cdk8s-pipeline',
-
-                                         source_action=cpactions.GitHubSourceAction(
-                                             action_name='Github',
-                                             output=source_artifact,
-                                             oauth_token=core.SecretValue.secrets_manager(
-                                                 'github-token'),
-                                             owner='donnieprakoso',
-                                             repo='tutorial-cdk-eks-ec2',
-                                             branch='main',
-                                             trigger=cpactions.GitHubTrigger.POLL),
-
-                                         synth_action=pipelines.SimpleSynthAction(
-                                             source_artifact=source_artifact,
-                                             cloud_assembly_artifact=cloud_assembly_artifact,
-                                             install_command='npm install -g aws-cdk && npm install -g cdk8s-cli && cd k8s-cluster-ec2 && pip install -r requirements.txt',
-                                             synth_command='cdk synth'))
+        pipeline = CodePipeline(self, "{}-pipeline".format(id),
+                                pipeline_name="{}-pipeline".format(id),
+                                synth=ShellStep("Synth",
+                                                input=CodePipelineSource.git_hub(
+                                                    "donnieprakoso/tutorial-cdk-eks-ec2", "main",
+                                                    authentication=core.SecretValue.secrets_manager(
+                                                        "github-token")
+                                                ),
+                                                commands=["npm install -g aws-cdk",
+                                                          "npm install -g cdk8s-cli",
+                                                          "cd k8s-cluster-ec2",
+                                                          "python -m pip install -r requirements.txt",
+                                                          "cdk synth"]
+                                                )
+                                )
